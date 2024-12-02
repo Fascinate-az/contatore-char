2ff01e1f572e4f33c9183b7d7358090daf886114
Fascinate <eddo92@yahoo.it>
2024-12-02 17:30:24 +0100

    Update contatore char.cs

--- contatore char.cs
+++ contatore char.cs
@@ -22,3 +22,28 @@ int numeri = 0;
 Console.WriteLine($"sono state trovate {vocali} vocali, {consonanti} consonanti, {numeri} numeri e {punteggiatura} simboli di punteggiatura per un totale di {frase.Length} caratteri");
 
 Console.ReadKey();
+
+
+////// conta anche i numeri nel nome del file
+
+
+DirectoryInfo cartella = new DirectoryInfo("C:\\Users\\eddo1\\Desktop\\File note");
+long spazioTotale = 0;
+int numeri = 0;
+
+foreach(FileInfo file in cartella.GetFiles())
+{   
+    foreach (char c in file.Name)
+    {
+        if (char.IsDigit(c))
+            numeri++;
+    }
+    Console.WriteLine($"File:{file.Name.PadRight(60, ' ')} - dimensioni:{file.Length} bytes ");
+    spazioTotale += file.Length;
+    numeri++;
+}
+
+Console.WriteLine($"\n-------------------- Totale spazio occuppato dalla cartella {spazioTotale} bytes e con {numeri} numeri");
+
+
+Console.ReadKey();
